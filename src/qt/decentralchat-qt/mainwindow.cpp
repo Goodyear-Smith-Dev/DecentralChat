72aafd156e0c439a669f43f9660285c2cb4ba103
Ryan Smith <rysmith2113@gmail.com>
2023-07-29 02:01:35 -0400

    Coding style changes

--- src/qt/decentralchat-qt/mainwindow.cpp
+++ src/qt/decentralchat-qt/mainwindow.cpp
@@ -1,15 +1,14 @@
 #include "mainwindow.h"
 #include "ui_decentralchat.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget* parent):
+	QMainWindow(parent),
+	ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
