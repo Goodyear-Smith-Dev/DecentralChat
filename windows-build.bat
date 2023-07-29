7c77a3146bf3101a3019c8a6c7ec898b3a43a9c6
SZB3748 <cubepiggy@gmail.com>
2023-07-29 19:24:37 -0400

    Add windows build script

---
+++ windows-build.bat
@@ -0,0 +1,6 @@
+mkdir build\.qm
+xcopy /S /Q /Y /F "src\qt\decentralchat-qt\DecentralChat_es_MX.ts" "build\.qm\"
+move "build\.qm\DecentralChat_es_MX.ts" "build\.qm\DecentralChat_es_MX.qm"
+cd build
+"C:\Qt\6.5.2\msvc2019_64\bin\qmake6.exe" ../src/qt/decentralchat-qt/DecentralChat.pro
+"C:\Qt\Tools\QtCreator\bin\jom\jom.exe"
