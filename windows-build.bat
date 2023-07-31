d32133d23935313ac297782f95e1c995920cf7b9
Ryan Smith <rysmith2113@gmail.com>
2023-07-30 23:54:33 -0400

    Add a comment

--- windows-build.bat
+++ windows-build.bat
@@ -1,3 +1,5 @@
+:: Build DecentralCHat for Windows
+
 mkdir build\.qm
 xcopy /S /Q /Y /F "src\qt\decentralchat-qt\DecentralChat_es_MX.ts" "build\.qm\"
 move "build\.qm\DecentralChat_es_MX.ts" "build\.qm\DecentralChat_es_MX.qm"
