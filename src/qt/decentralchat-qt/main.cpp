f8df1c04d695bf7ac6a62d86c55359bdd6d006e9
Ryan Smith <rysmith2113@gmail.com>
2023-08-09 13:29:27 -0400

    Add some test code

--- src/qt/decentralchat-qt/main.cpp
+++ src/qt/decentralchat-qt/main.cpp
@@ -1,10 +1,15 @@
 #include "mainwindow.hpp"
+#include "rsautil.hpp"
+#include "aesutil.hpp"
+#include "hashing.hpp"
 
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
 #include <QString>
 #include <QStringList>
+
+namespace aes = decentralchat::aes;
 
 int main(int argc, char *argv[])
 {
@@ -22,5 +27,40 @@ int main(int argc, char *argv[])
 
     MainWindow window;
     window.show();
-    return app.exec();
+
+	// auto&& [keys, publicKey, privateKey] = rsa::generateKeys();
+	// rsa::saveKey(publicKey, "pubkey.key");
+
+	// CryptoPP::ByteQueue queue;
+	// publicKey.Save(queue);
+	// rsautil::saveBase64("pubkey.key", queue);
+
+	// queue.Clear();
+
+	// privateKey.Save(queue);
+	// rsautil::saveBase64("privkey.key", queue);
+
+	std::string message = "Hello, World!";
+	// auto signature = rsautil::sign(message, privateKey);
+	// bool verified = rsautil::verify(message, signature, publicKey);
+	// std::cout << verified << "\n";
+
+	// auto ciphertext = rsautil::encrypt(message, publicKey);
+	// auto recovered = rsautil::decrypt(ciphertext, privateKey);
+	// std::cout << recovered << "\n";
+
+	auto&& [ciphertext, key, iv] = aes::encrypt(message);
+	std::cout << key.BytePtr() << "\n";
+	auto recovered = aes::decrypt(ciphertext, key, iv);
+	std::cout << recovered << "\n";
+
+	auto hexData = aes::dataToHex(ciphertext, key, iv);
+	std::cout << hexData.ciphertext << "\n" << hexData.key << "\n" << hexData.iv << "\n";
+
+	auto data = aes::dataFromHex(hexData);
+	std::cout << data.ciphertext << "\n" << data.key.BytePtr() << "\n" << data.iv.BytePtr() << "\n";
+
+	std::cout << decentralchat::hashing::SHA256("abc") << "\n";
+
+    return 0;//app.exec();
 }
